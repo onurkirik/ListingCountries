e1f4e59a68bb7639ef7df26dee03529b0d2f5647
Onur Kırık <kirikonurr@gmail.com>
2023-01-07 01:45:22 +0300

    Add project files.

---
+++ UlkelerDunyasi/DATA/Ulke.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UlkelerDunyasi.DATA
+{
+    public class Ulke
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Ad { get; set; } = null!;
+    }
+}
---
+++ UlkelerDunyasi/DATA/UygulamaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UlkelerDunyasi.DATA
+{
+    public class UygulamaDbContext:DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ulke> Ulkeler => Set<Ulke>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ulke>().HasData(
+                new Ulke() { Id=1, Ad="Kanada"},
+                new Ulke() { Id=2, Ad="ABD"},
+                new Ulke() { Id=3, Ad="UK"},
+                new Ulke() { Id=4, Ad="Avustralya"});
+        }
+    }
+}
